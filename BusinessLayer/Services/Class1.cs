fc2647ab5efef1ad3389c619f175a27ce4132114
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:18:35 +0530

    files added

---
+++ BusinessLayer/Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    class Class1
+    {
+       
+
+    }
+}
---
+++ Level3/Controllers/NewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level3.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+           // Implement to delete data from the database using Entity Framework core API.
+                //a.Create web API delete method.
+       //b.Use entry method to set deleted state.
+       //c.Use method to handleobject graph deletes.
+
+
+            return View();
+        }
+    }
+}
